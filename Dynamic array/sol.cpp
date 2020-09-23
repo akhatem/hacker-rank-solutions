364415806d5e2ded63bc0cd505428eefd2193f9f
Ahmed Kareem <31676293+akhatem@users.noreply.github.com>
2020-09-24 01:31:29 +0200

    Add files via upload

---
+++ Dynamic array/sol.cpp
@@ -0,0 +1,119 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'dynamicArray' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. 2D_INTEGER_ARRAY queries
+ */
+
+vector<int> dynamicArray(int n, vector<vector<int>> queries) {
+	vector<vector<int> > seq(n);
+	int lastans=0;
+	int len=queries.size();
+	vector<int> ans;
+	for(int i=0;i<len;i++){
+		int x=queries[i][1], y=queries[i][2];
+		int ind=(x^lastans)%n;
+		if(queries[i][0]==1) seq[ind].push_back(y);
+		else{
+			lastans=seq[ind][y%seq[ind].size()];
+			ans.push_back(lastans);
+		}
+	}
+	return ans;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string first_multiple_input_temp;
+    getline(cin, first_multiple_input_temp);
+
+    vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+    int n = stoi(first_multiple_input[0]);
+
+    int q = stoi(first_multiple_input[1]);
+
+    vector<vector<int>> queries(q);
+
+    for (int i = 0; i < q; i++) {
+        queries[i].resize(3);
+
+        string queries_row_temp_temp;
+        getline(cin, queries_row_temp_temp);
+
+        vector<string> queries_row_temp = split(rtrim(queries_row_temp_temp));
+
+        for (int j = 0; j < 3; j++) {
+            int queries_row_item = stoi(queries_row_temp[j]);
+
+            queries[i][j] = queries_row_item;
+        }
+    }
+
+    vector<int> result = dynamicArray(n, queries);
+
+    for (int i = 0; i < (int)result.size(); i++) {
+        fout << result[i];
+
+        if (i != (int)result.size() - 1) {
+            fout << "\n";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
